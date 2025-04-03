4b29e24d2336e33165c8b2fbdbd1482f9bac0546
iamAbishekRavi <abishek29080@gmail.com>
2025-04-03 12:41:00 +0530

    [Get Request]

--- app/main.js
+++ app/main.js
@@ -1,12 +1,25 @@
-// Simple HTTP server using Node.js raw socket approach
+// HTTP server using Node.js raw socket approach with path-based routing
 const net = require('net');
 
 // Create TCP server
 const server = net.createServer((socket) => {
   // Listen for incoming data
-  socket.on('data', () => {
-    // Send exactly the specified response format
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+  socket.on('data', (data) => {
+    // Parse the HTTP request to extract the path
+    const request = data.toString();
+    
+    // Simple regex to extract the path from the request
+    const pathMatch = request.match(/GET\s+(\/[^\s]*)\s+HTTP/);
+    const path = pathMatch ? pathMatch[1] : null;
+    
+    if (path === '/') {
+      // Root path: return 200 OK
+      socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    } else {
+      // Any other path: return 404 Not Found
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    }
+    
     // Close the connection after sending the response
     socket.end();
   });
