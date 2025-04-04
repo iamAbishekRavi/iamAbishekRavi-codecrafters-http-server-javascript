7a4a851f1bc890796ca16241d56f354291466564
iamAbishekRavi <abishek29080@gmail.com>
2025-04-04 09:38:36 +0530

    [Get Request123]

--- app/main.js
+++ app/main.js
@@ -3,21 +3,18 @@ const net = require('net');
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
-        // Match the "/user-agent" request
-        const userAgentMatch = request.match(/GET \/user-agent HTTP/);
-        if (userAgentMatch) {
-            // Extract "User-Agent" value from headers
-            const userAgentHeader = request.match(/User-Agent: (.+)\r\n/);
-            if (userAgentHeader) {
-                const userAgentValue = userAgentHeader[1];
-                const contentLength = Buffer.byteLength(userAgentValue, 'utf-8');
+        
+        // Match the request for "/echo/{string}"
+        const echoMatch = request.match(/^GET \/echo\/([^ ]+) HTTP/);
+        if (echoMatch) {
+            const responseString = echoMatch[1];
+            const contentLength = Buffer.byteLength(responseString, 'utf-8');
 
-                const responseHeaders = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n`;
-
-                socket.write(responseHeaders + userAgentValue);
-                socket.end();
-                return;
-            }
+            const responseHeaders = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n`;
+            
+            socket.write(responseHeaders + responseString);
+            socket.end();
+            return;
         }
 
         // Handle unknown routes with 404
